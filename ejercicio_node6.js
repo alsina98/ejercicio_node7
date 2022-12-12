36448c59ef78822d25afb9d33341254510e2e5ba
Jordi <91033826+alsina98@users.noreply.github.com>
2022-12-12 13:12:36 +0100

    Create ejercicio_node6.js

---
+++ ejercicio_node6.js
@@ -0,0 +1,13 @@
+let familia = ["Jordi", "Victoria", "Josep", "Teresa", "Concep", "Noelia", "Marta", "Ramon", "Xavi"];
+
+const miFamilia = new Set(familia);
+
+console.log(miFamilia);
+
+miFamilia.add("Jordi");
+
+console.log(miFamilia);
+
+miFamilia.add("JavaScript");
+
+console.log(miFamilia);
